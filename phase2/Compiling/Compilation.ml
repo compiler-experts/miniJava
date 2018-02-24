a38536e4726f63050a160a5ab5c108d944b749fb
ZHOU Xiaofeng <xiaofeng.zhou@telecom-bretagne.eu>
2018-02-24 16:34:10 +0100

    compilation : add verification of the name of function in the two tables. functions can not have the same name

--- phase2/Compiling/Compilation.ml
+++ phase2/Compiling/Compilation.ml
@@ -18,38 +18,6 @@ type globalData =
   classTable : (string, globalClassDescriptor) Hashtbl.t;
 }
 
-let printClassDescriptor globalClassDescriptor  =
-	match globalClassDescriptor with
-	| ClassDescriptor cd ->
-			Hashtbl.iter (fun key value ->print_string("\t\t");print_string(key ^ " : " );print_endline(value)) cd.methods
-
-let printClassTable classTable =
-	print_endline("list of the classes ：");
-	Hashtbl.iter (fun key value ->print_string("\t");print_endline(key);printClassDescriptor(value)) classTable
-
-
-let addMethodsToClassDesciptor className methods cmethod =
-	let nameMethod = className ^ "_" ^ cmethod.mname in
-	Hashtbl.add methods cmethod.mname nameMethod
-
-
-let addMethodsToMethodTable className methodTable cmethod =
-	let nameMethod = className ^ "_" ^ cmethod.mname in
-	Hashtbl.add methodTable nameMethod cmethod
-
-(*
-		ulity: add methods in the Hashtbl classTable
-*)
-let addToClassTable classTable className c =
-  let methodsClass = Hashtbl.create 20 in
-  List.iter (addMethodsToClassDesciptor className methodsClass) c.cmethods;
-	Hashtbl.add classTable className (ClassDescriptor({name=className;methods=methodsClass}))
-
-(*
-	ulity: add methods in the Hashtbl methodTable
-*)
-let addToMethodTable methodTable className c =
-	List.iter (addMethodsToMethodTable className methodTable) c.cmethods
 
 
 (*verify if a key exists in the table
@@ -60,6 +28,61 @@ let verifyHashtbl nameHashtbl key =
   match key with
   | _ -> Hashtbl.mem nameHashtbl key
   | "Object" -> true
+
+let printMethodTable methodTable =
+	print_endline("list of the methods ：");
+	Hashtbl.iter (fun key value ->print_string("\t");print_string(key ^ " : ");print_endline(value.mname)) methodTable
+
+
+let printClassDescriptor globalClassDescriptor  =
+	match globalClassDescriptor with
+	| ClassDescriptor cd ->
+			Hashtbl.iter (fun key value ->print_string("\t\t");print_string(key ^ " : " );print_endline(value)) cd.methods
+
+let printClassTable classTable =
+	print_endline("list of the classes ：");
+	Hashtbl.iter (fun key value ->print_string("\t");print_endline(key);printClassDescriptor(value)) classTable
+
+
+
+(*
+		ulity: add methods in the Hashtbl classTable
+*)
+let addMethodsToClassDesciptor className methods cmethod =
+	if(verifyHashtbl methods  cmethod.mname) = false
+	then begin
+		let nameMethod = className ^ "_" ^ cmethod.mname in
+		Hashtbl.add methods cmethod.mname nameMethod
+	end
+	else begin
+		print_endline("function " ^ cmethod.mname ^ " already defined")
+	end
+
+let addToClassTable classTable className c =
+  let methodsClass = Hashtbl.create 20 in
+  List.iter (addMethodsToClassDesciptor className methodsClass) c.cmethods;
+	Hashtbl.add classTable className (ClassDescriptor({name=className;methods=methodsClass}))
+
+(*
+	ulity: add methods in the Hashtbl methodTable
+*)
+
+let addMethodsToMethodTable className methodTable cmethod =
+	let nameMethod = className ^ "_" ^ cmethod.mname in
+	if(verifyHashtbl methodTable  nameMethod) = false
+	then begin
+		Hashtbl.add methodTable nameMethod cmethod
+	end
+	else begin
+		print_endline("function " ^ cmethod.mname ^ " already in the method table")
+	end
+
+
+let addToMethodTable methodTable className c =
+	List.iter (addMethodsToMethodTable className methodTable) c.cmethods
+
+
+
 
 
 (*asttype ={  mutable modifiers : modifier list; id : string; info : type_info;}
@@ -75,7 +98,11 @@ let compileClass methodTable classTable ast asttype =
   | Inter -> ()
 
 
+(*
+	methodTable: is used to find the content of a function according to the name
+*)
 let compileAST ast =
   let programData = { methodTable = Hashtbl.create 20; classTable = Hashtbl.create 20 } in
   List.iter (compileClass programData.methodTable programData.classTable ast) ast.type_list;
-	printClassTable programData.classTable
+	printClassTable programData.classTable;
+	printMethodTable programData.methodTable;
