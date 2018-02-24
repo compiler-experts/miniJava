062b4d3584551d3223ead9ab93f385e4a0e9443b
ZHOU Xiaofeng <xiaofeng.zhou@telecom-bretagne.eu>
2018-02-24 15:56:02 +0100

    compilation: finish printClassTable and printClassDescriptor functions to display the content of a class

--- phase2/Compiling/Compilation.ml
+++ phase2/Compiling/Compilation.ml
@@ -18,28 +18,33 @@ type globalData =
   classTable : (string, globalClassDescriptor) Hashtbl.t;
 }
 
+let printClassDescriptor globalClassDescriptor  =
+	match globalClassDescriptor with
+	| ClassDescriptor cd ->
+			Hashtbl.iter (fun key value ->print_string("\t\t");print_string(key ^ " : " );print_endline(value)) cd.methods
+
+let printClassTable classTable =
+	print_endline("list of the classes ：");
+	Hashtbl.iter (fun key value ->print_string("\t");print_endline(key);printClassDescriptor(value)) classTable
+
 
 let addMethodsToClassDesciptor className methods cmethod =
 	let nameMethod = className ^ "_" ^ cmethod.mname in
-	Hashtbl.add methods cmethod.mname nameMethod;
-	print_endline("add method to classtable :" ^ className ^ nameMethod ^ cmethod.mname)
+	Hashtbl.add methods cmethod.mname nameMethod
 
 
 let addMethodsToMethodTable className methodTable cmethod =
 	let nameMethod = className ^ "_" ^ cmethod.mname in
-	Hashtbl.add methodTable nameMethod cmethod;
-	print_endline("add method to methodsTable" ^ className ^ nameMethod ^ cmethod.mname)
+	Hashtbl.add methodTable nameMethod cmethod
 
 
 let addToClassTable classTable className c =
-  print_endline("add class to class table" ^ className);
   let methodsClass = Hashtbl.create 20 in
   List.iter (addMethodsToClassDesciptor className methodsClass) c.cmethods;
 	Hashtbl.add classTable className (ClassDescriptor({name=className;methods=methodsClass}))
 
 
 let addToMethodTable methodTable className c =
-  print_endline("add methods to method table of class" ^ className);
 	List.iter (addMethodsToMethodTable className methodTable) c.cmethods
 
 
@@ -58,10 +63,13 @@ let compileClass methodTable classTable ast asttype =
   match asttype.info with
   | Class c -> if(verifyHashtbl classTable asttype.id) = false
                 then begin  addToClassTable classTable asttype.id c;
-                            addToMethodTable methodTable asttype.id c end
+                            addToMethodTable methodTable asttype.id c
+														 end
+
   | Inter -> ()
 
 
 let compileAST ast =
   let programData = { methodTable = Hashtbl.create 20; classTable = Hashtbl.create 20 } in
   List.iter (compileClass programData.methodTable programData.classTable ast) ast.type_list;
+	printClassTable programData.classTable
