53b39050bac804c77d7d331e86e7e00c6f32ff31
ZHOU Xiaofeng <xiaofeng.zhou@telecom-bretagne.eu>
2018-02-25 02:34:54 +0100

    modify .merlin Compile.ml and _tags to test the compilation

--- phase2/Main/Compile.ml
+++ phase2/Main/Compile.ml
@@ -1,15 +1,19 @@
 open Parser
 
-let execute lexbuf verbose = 
-  try 
+let execute lexbuf verbose =
+  try
     let ast = compilationUnit Lexer.token lexbuf in
     print_endline "successfull parsing";
-    if verbose then AST.print_program ast;
+
+    (*if verbose then AST.print_program ast;
     print_endline "===================";
     Typing.typing ast;
     if verbose then AST.print_program ast;
-    print_endline "successfull typing";
-  with 
+    print_endline "successfull typing";*)
+    Compilation.compileAST ast;
+    print_endline "compile successfull";
+
+  with
     | Error ->
       print_string "Syntax error: ";
       Location.print (Location.curr lexbuf)
